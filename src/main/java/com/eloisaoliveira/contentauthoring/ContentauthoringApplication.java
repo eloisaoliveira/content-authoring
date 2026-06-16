68a045aef86402e296e1c329e3fd00ff0b7d4877
Eloisa Oliveira <eloisa.oliveirasantos@gmail.com>
2026-06-16 20:43:23 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/eloisaoliveira/contentauthoring/ContentauthoringApplication.java
@@ -0,0 +1,13 @@
+package com.eloisaoliveira.contentauthoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentauthoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentauthoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eloisaoliveira/contentauthoring/ContentauthoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eloisaoliveira.contentauthoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentauthoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
